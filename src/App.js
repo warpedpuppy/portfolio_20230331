e25fad40bdfabfdebb0533e6404e6ae424dc8b7e
ted <ted@warpedpuppy.com>
2023-03-31 13:52:40 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.scss';
+import Logo from './components/Logo';
+import './animations/animations.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+		<section id="home">
+			<div id="home-box">
+				<fieldset><legend>home</legend></fieldset>
+				<fieldset><legend>c.v.</legend></fieldset>
+				<fieldset><legend>fun stuff</legend></fieldset>
+				<fieldset><legend>contact</legend></fieldset>
+			</div>
+			<Logo />
+		</section>
     </div>
   );
 }
