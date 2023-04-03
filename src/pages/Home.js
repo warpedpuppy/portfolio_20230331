5ef6d3b8fe26eb624dc008feb587a9da31680e24
ted <ted@warpedpuppy.com>
2023-04-03 08:42:36 -0400

    simplified and fixed layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,19 @@
 import Logo from '../components/Logo';
 import './Home.scss';
 function Home({ setPage }) {
+ 
+  const year = new Date().getFullYear();
+  const years = year - 2004;	
+
   return (
     <section id="home">
       <Logo />
       <div id="home-box">
-        <fieldset>
-          <legend>home</legend>
-        </fieldset>
-        <fieldset>
-          <legend onClick={ () => setPage('CV')}>c.v.</legend>
-        </fieldset>
-        <fieldset>
-          <legend onClick={ () => setPage('fun-stuff')}>fun stuff</legend>
-        </fieldset>
-        <fieldset>
-          <legend onClick={ () => setPage('contact')}>contact</legend>
-        </fieldset>
+		<p>I am Ted, a software engineer with {years} years experience.</p>
+		<ul>
+			<li><h3 onClick={ () => setPage('CV')}>c.v.</h3></li>
+			<li><h3 onClick={ () => setPage('contact')}>contact</h3></li>
+		</ul>
       </div>
     </section>
   );
